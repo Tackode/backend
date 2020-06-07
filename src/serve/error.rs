4d421a1ebfb596809eb2eafd0cc1054b78bd9f1d
Julien Blatecky <julien.blatecky@creatiwity.net>
2020-06-07 12:57:15 +0200

    Improve error handling

--- src/serve/error.rs
+++ src/serve/error.rs
@@ -18,40 +18,46 @@ struct ErrorResponse {
     message: String,
 }
 
-pub async fn handle_rejection(err: Rejection) -> Result<impl Reply, Infallible> {
-    let code;
-    let message;
+struct InternalErrorResponse {
+    code: StatusCode,
+    message: String,
+}
 
-    if err.is_not_found() {
-        code = StatusCode::NOT_FOUND;
-        message = String::from("NOT_FOUND");
-    } else if let Some(error) = err.find::<Error>() {
-        code = match error {
-            Error::InvalidData => StatusCode::BAD_REQUEST,
-            Error::Unauthorized => StatusCode::UNAUTHORIZED,
-            // Error::ModelError { source } => match source {
-            //     Model::error::Error::ModelNotFound => StatusCode::NOT_FOUND,
-            //     _ => StatusCode::INTERNAL_SERVER_ERROR,
-            // },
-        };
+pub async fn handle_rejection(err: Rejection) -> Result<impl Reply, Rejection> {
+    let response: Result<InternalErrorResponse, Rejection>;
 
-        message = error.to_string();
-    } else if let Some(_) = err.find::<warp::reject::MissingHeader>() {
-        code = StatusCode::UNAUTHORIZED;
-        message = String::from("Unauthorized");
-    } else if let Some(_) = err.find::<warp::reject::MethodNotAllowed>() {
-        code = StatusCode::METHOD_NOT_ALLOWED;
-        message = String::from("METHOD_NOT_ALLOWED");
+    if let Some(error) = err.find::<Error>() {
+        response = Ok(InternalErrorResponse {
+            code: match error {
+                Error::InvalidData => StatusCode::BAD_REQUEST,
+                Error::Unauthorized => StatusCode::UNAUTHORIZED,
+                // Error::ModelError { source } => match source {
+                //     Model::error::Error::ModelNotFound => StatusCode::NOT_FOUND,
+                //     _ => StatusCode::INTERNAL_SERVER_ERROR,
+                // },
+            },
+            message: error.to_string(),
+        });
+    } else if let Some(missing_header) = err.find::<warp::reject::MissingHeader>() {
+        if missing_header.name() == "authorization" {
+            response = Ok(InternalErrorResponse {
+                code: StatusCode::UNAUTHORIZED,
+                message: String::from("Unauthorized"),
+            })
+        } else {
+            response = Err(err);
+        }
     } else {
-        log::error!("unhandled rejection: {:?}", err);
-        code = StatusCode::INTERNAL_SERVER_ERROR;
-        message = String::from("UNHANDLED_REJECTION");
+        response = Err(err);
     }
 
-    let json = warp::reply::json(&ErrorResponse {
-        code: code.as_u16(),
-        message: message.into(),
-    });
-
-    Ok(warp::reply::with_status(json, code))
+    response.map(|error| {
+        warp::reply::with_status(
+            warp::reply::json(&ErrorResponse {
+                code: error.code.as_u16(),
+                message: error.message,
+            }),
+            error.code,
+        )
+    })
 }
